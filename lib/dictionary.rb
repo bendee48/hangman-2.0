4236c83267d722d464ef5761635bb36761c7313d
Ben Daniels <benbanj@hotmail.com>
2020-09-02 15:44:01 +0100

    Adding more dictionary tests

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -2,10 +2,19 @@ class Dictionary
   attr_reader :file
   
   def initialize(file)
-    @file = File.open("spec/#{file}")
+    @file = File.open("static/#{file}")
   end
 
-  def self.word
-    "hello"
+  def word
+    words.sample
+  end
+
+  private
+
+  def words
+    @words ||= file.readlines(chomp: true)
+                   .reject do |word|
+                      word.size < 5 || word.size > 12
+                    end
   end
 end
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -8,20 +8,21 @@ RSpec.describe Dictionary do
     expect(subject.file).to be_a File 
   end
 
-  describe '.word' do
+  describe '#word' do
     it 'returns a string' do
-      expect(described_class.word).to be_a String
+      expect(subject.word).to be_a String
     end
 
     it 'returns a string only containing letters' do
-      word = described_class.word
+      word = subject.word
       expect(word).to satisfy('only contain letters') do 
         word.chars.all? { |letter| letter =~ /[a-z]/i }
       end
     end
 
-    it 'returns a random word between 5 and 12 characters' do
-      expect(described_class.word.size).to be >=5 and be <= 12
-    end    
+    it 'returns a word between 5 and 12 characters' do
+      expect(subject.word.size).to be >=5 and be <= 12
+    end
   end
+
 end
