e7db0c38a11aeaa6c4c12eb96ff1e89fc5398e99
Ben Daniels <benbanj@hotmail.com>
2020-10-05 17:51:20 +0100

    Adding savegame dir creation

--- lib/game_save.rb
+++ lib/game_save.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 require 'yaml'
+require 'fileutils'
 
 # Class to save and load game
 class GameSave
   def self.save(game)
     game_data = YAML.dump(game)
     save_file = File.join(File.dirname(__FILE__), '../lib/save_games/savegame.txt')
+    dirname = File.dirname(save_file)
+
+    unless File.directory?(dirname)
+      FileUtils.mkdir(dirname)
+    end
+
     File.open(save_file, 'w') do |f|
       f.write game_data
     end
